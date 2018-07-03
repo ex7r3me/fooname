24db740797587cb8b24e55da29d9b4c2da52dc92
ex7r3me <siavash.ahmadpour@gmail.com>
2018-07-03 15:06:50 +0430

    Add open weather API and update on login
    - simple clean up and standard style guide
    - only run on port 8080 
    - add weather API 
    - update user name after login with simple text from Tehran (hard-coded) weather

--- server.js
+++ server.js
@@ -1,97 +1,90 @@
 const http = require('http')
 const Twit = require('twit')
 
-var express = require('express');
-var passport = require('passport');
-var Strategy = require('passport-twitter').Strategy;
+const express = require('express')
+const passport = require('passport')
+const Strategy = require('passport-twitter').Strategy
+const rp = require('request-promise')
+const _ = require('lodash')
 
-
+const twitterConsumerKey = process.env.CONSUMER_KEY || ''
+const twitterConsumerSecret = process.env.CONSUMER_SECRET || ''
 passport.use(new Strategy({
-        consumerKey: process.env.CONSUMER_KEY,
-        consumerSecret: process.env.CONSUMER_SECRET,
-        callbackURL: 'http://127.0.0.1:3000/login/twitter/return'
-    },
-    function(token, tokenSecret, profile, cb) {
-        console.log('>',token)
-        console.log('>>',tokenSecret)
-        // In this example, the user's Twitter profile is supplied as the user
-        // record.  In a production-quality application, the Twitter profile should
-        // be associated with a user record in the application's database, which
-        // allows for account linking and authentication with other identity
-        // providers.
-        return cb(null, profile);
-    }));
+  consumerKey: twitterConsumerKey,
+  consumerSecret: twitterConsumerSecret,
+  callbackURL: 'http://127.0.0.1:3000/login/twitter/return'
+},
+function (token, tokenSecret, profile, cb) {
+  console.log('gonna get here')
+  return cb(null, profile)
+}))
 
-passport.serializeUser(function(user, cb) {
-    cb(null, user);
-});
+passport.serializeUser(function (user, cb) {
+  cb(null, user)
+})
 
-passport.deserializeUser(function(obj, cb) {
-    cb(null, obj);
-});
-
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj)
+})
 
 // Create a new Express application.
-var app = express();
+var app = express()
 
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(require('morgan')('combined'))
+app.use(require('cookie-parser')())
+app.use(require('body-parser').urlencoded({ extended: true }))
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }))
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-
-const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
-passport.use(new Strategy({
-        consumerKey: process.env.CONSUMER_KEY,
-        consumerSecret: process.env.CONSUMER_SECRET,
-        callbackURL: 'http://127.0.0.1:3000/login/twitter/return'
-    },
-    function(token, tokenSecret, profile, cb) {
-        console.log('>',token)
-        console.log('>>',tokenSecret)
-        return cb(null, profile);
-    }));
-
-const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Hello Node.js Server!')
+const serverPort = process.env.OPENSHIFT_NODEJS_PORT || 8080
+//const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+let weatherRequestOptions = {
+  uri: 'https://api.openweathermap.org/data/2.5/weather',
+  qs: {
+    id: '112931',
+    appId: '3235997ff734458b6e4a14e904a37707'
+  },
+  json: true
 }
-const server = http.createServer(requestHandler)
-server.listen(server_port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-    console.log(`server is listening on ${server_port}`)
-})
-let T = new Twit({
-    consumer_key:         process.env.CONSUMER_KEY,
-    consumer_secret:      process.env.CONSUMER_SECRET,
-    access_token:         '',
-    access_token_secret:  '',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            true,     // optional - requires SSL certificates to be valid.
-})
+passport.use(new Strategy({
+  consumerKey: twitterConsumerKey,
+  consumerSecret: twitterConsumerSecret,
+  callbackURL: 'http://127.0.0.1:8080/login/twitter/return'
+},
+function (token, tokenSecret, profile, cb) {
+  let T = new Twit({
+    consumer_key: twitterConsumerKey,
+    consumer_secret: twitterConsumerSecret,
+    access_token: token,
+    access_token_secret: tokenSecret,
+    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+    strictSSL: true // optional - requires SSL certificates to be valid.
+  })
 
-//
-//  tweet 'hello world!'
-//
-T.post('account/update_profile', { name: `smile is here` }, function(err, data, response) {
-    console.log(data)
-})
+  rp(weatherRequestOptions)
+    .then(function (weather) {
+      let weatherStatus = _.get(weather, 'weather[0].main', '')
+      T.post('account/update_profile', { name: `Smile is so ${weatherStatus}` }, (err, data, response) => {
+        console.log(data)
+      })
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+
+  return cb(null, profile)
+}))
+
 app.get('/login/twitter',
-    passport.authenticate('twitter'));
+  passport.authenticate('twitter'))
 
 app.get('/login/twitter/return',
-    passport.authenticate('twitter', { failureRedirect: '/login' }),
-    function(req, res) {
-        res.redirect('/');
-    });
-app.listen(3000);
-
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect('/')
+  })
+app.listen(serverPort)
