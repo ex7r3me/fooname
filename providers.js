46f9a006a7208f7009545c318b39222df490a347
ex7r3me <siavash.ahmadpour@gmail.com>
2018-07-09 14:49:05 +0430

    update provider.js
    - add BASE_URL

--- providers.js
+++ providers.js
@@ -4,7 +4,7 @@ module.exports = {
     'module': 'passport-twitter',
     'consumerKey': process.env.CONSUMER_KEY,
     'consumerSecret': process.env.CONSUMER_SECRET,
-    'callbackURL': 'http://localhost:8080/api/auth/twitter/callback',
+    'callbackURL': `${process.env.BASE_URL}/api/auth/twitter/callback`,
     'authPath': '/api/auth/twitter',
     'callbackPath': '/api/auth/twitter/callback',
     'successRedirect': '/api/auth/account'
